5ea37b71dc1d55560dd3956231700f764be072ef
muhammad zidan sembiring <zidanmuh564@gmail.com>
2025-11-21 16:12:34 +0700

    Add files via upload

---
+++ build/abdi_pkg/CMakeFiles/abdinode.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/abdi/src/abdi_pkg/src/abdinode.cpp" "CMakeFiles/abdinode.dir/src/abdinode.cpp.o" "gcc" "CMakeFiles/abdinode.dir/src/abdinode.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/abdi_pkg/CMakeFiles/abdinode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abdinode.dir/src/abdinode.cpp.o"
+  "CMakeFiles/abdinode.dir/src/abdinode.cpp.o.d"
+  "abdinode"
+  "abdinode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abdinode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
